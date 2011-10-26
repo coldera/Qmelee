ffba56556e86ba5ba8a8ce6a3038e2623aaf7402
coldera <yikehao@gmail.com>
2011-10-26 22:09:30 +0800

    ai for general ken

--- lua/ai/melee-sf-ai.lua
+++ lua/ai/melee-sf-ai.lua
@@ -92,8 +92,82 @@ sgs.ai_skill_use["@@bodong"]=function(self,prompt)
     
 	self:sort(self.enemies,"defense")    
     for _,enemy in ipairs(self.enemies) do
-        if self.player:distanceTo(enemy) <= self.player:getAttackRange()+1 then
+        if self.player:distanceTo(enemy) <= self.player:getAttackRange()+1 and not self:isEquip("HolyWing", enemy) then
             return "@BodongCard="..slash.."->"..enemy:objectName()
+        end
+    end
+    
+    return "."    
+end
+
+-- ken ---------------------------------------------------------------------------------
+
+sgs.ai_chaofeng["ryu"] = 4
+
+sgs.ai_card_intention["JifengCard"]=function(card,from,to,source)
+    return sgs.ai_card_intention.general(to,80)
+end
+sgs.ai_card_intention["ShenlongCard"]=function(card,from,to,source)
+    return sgs.ai_card_intention.general(to,80)
+end
+
+sgs.dynamic_value.damage_card["JifengCard"] = true
+sgs.dynamic_value.damage_card["ShenlongCard"] = true
+
+-- jifeng
+sgs.ai_skill_choice.jifeng = function(self, choices)
+    local slash_num = self:getCardsNum("Slash")
+    
+    if slash_num<=0 then 
+        return "NoChoice" 
+    elseif slash_num==1 then
+        return "JifengEffect1" 
+    else
+        return "JifengEffect2"
+    end
+    
+end
+
+sgs.ai_skill_use["@@jifeng"]=function(self,prompt)
+    if self:getCardsNum("Slash")<=0 then return "." end
+    
+    local slash
+    
+	self:sort(self.enemies,"defense")    
+    
+    if self.player:getMark("jifeng")==1 then
+    
+        for _,enemy in ipairs(self.enemies) do
+            if self.player:distanceTo(enemy)<=1 and enemy:hasFlag("jifenged") then
+                slash = self:getCardId("Slash")
+                return "@JifengCard="..slash.."->"..enemy:objectName()
+            end
+        end
+        
+    elseif  self.player:getMark("jifeng")==2 then
+        
+        for _,enemy in ipairs(self.enemies) do
+            if self.player:distanceTo(enemy)<=1 and not enemy:hasFlag("jifenged") then
+                slash = self:getCardId("Slash")
+                return "@JifengCard="..slash.."->"..enemy:objectName()
+            end
+        end
+        
+    end
+    
+    return "."    
+end
+
+-- shenglong
+sgs.ai_skill_use["@@shenglong"]=function(self,prompt)
+    if self:getCardsNum("Slash")<=0 or self.player:getMp()<2 then return "." end
+    
+    local slash = self:getCardId("Slash")
+    
+	self:sort(self.enemies,"defense")    
+    for _,enemy in ipairs(self.enemies) do
+        if self.player:inMyAttackRange(enemy) and not self:isEquip("HolyWing", enemy) then
+            return "@ShenglongCard="..slash.."->"..enemy:objectName()
         end
     end
     
